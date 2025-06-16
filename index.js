449bc4c7f32fdccc33475120e1b0722a35a61d42
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-06-16 11:48:59 +0600

    somthing

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ import { errorHandler } from './utils/errorHandler.js';
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Serve static files for preview
 app.use('/preview', express.static(serverConfig.GENERATED_SITES_DIR));
 
-// Routes
 app.use('/api', siteRoutes);
 
-// Error handling
 app.use(errorHandler);
 
 app.listen(serverConfig.PORT, () => {
